00ac9827750e03ec7a1c16c1a38dbafcb1cadb8a
pengjianlin <skyland-zero@outlook.com>
2023-11-28 15:57:22 +0800

    新增 使用Scrutor扫描注入应用服务2

---
+++ src/Services/IdentityService/Cits.IdentityService.Application.Contracts/Users/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using Cits.Core.Dto;
+using Cits.IdentityService.Application.Contracts.User.Dto;
+
+namespace Cits.IdentityService.Application.Contracts.User;
+
+public interface IUserService
+{
+    Task<IResultOutput> CreateUserAsync(Domain.Users.User user, CancellationToken cancellationToken);
+}
---
+++ src/Services/IdentityService/Cits.IdentityService.Application/Users/UserService.cs
@@ -0,0 +1,37 @@
+﻿using Cits.Core.Dto;
+using Cits.IdentityService.Application.Contracts.User;
+using Cits.IdentityService.Domain.Shared.Users;
+using Cits.IdentityService.Domain.Users;
+using Microsoft.AspNetCore.Identity;
+
+namespace Cits.IdentityService.Application.Users;
+
+public class UserService : IUserService
+{
+    private readonly IFreeSql _freeSql;
+    private readonly IPasswordHasher<User> _passwordHasher;
+
+    public UserService(IFreeSql freeSql, IPasswordHasher<User> passwordHasher)
+    {
+        _freeSql = freeSql;
+        _passwordHasher = passwordHasher;
+    }
+
+    public async Task<IResultOutput> CreateUserAsync(User user, CancellationToken cancellationToken)
+    {
+        user.Id = Guid.NewGuid().ToString();
+        HashPassword(user);
+        await _freeSql.Insert(user).ExecuteAffrowsAsync(cancellationToken);
+        return ResultOutput.Ok();
+    }
+
+    private void HashPassword(User user)
+    {
+        switch (user.PasswordEncryptType)
+        {
+            case PasswordEncryptType.PasswordHasher:
+                user.Password = _passwordHasher.HashPassword(user, user.Password);
+                break;
+        }
+    }
+}
